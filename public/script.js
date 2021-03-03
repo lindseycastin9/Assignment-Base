c1537e6906b5962426531202eb69e123ccb1095e
Kyle Rosenstein <30937980+krosenstein@users.noreply.github.com>
2021-03-03 13:44:02 -0500

    Assignment

--- public/script.js
+++ public/script.js
@@ -1,1 +1,23 @@
+async function windowActions() {
+    console.log('window loaded');
+    const form = document.querySelector('.userform');
+    const search = document.querySelector('#zip');
 
+    const request = await fetch('/api');
+    const data = await request.json();
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        console.log('submit fired');
+        const dislpay = data.filter((record) => record.city === search.value);
+
+        console.table(data.data);
+
+    })
+    search.addEventListener('input', (event) => {
+        console.log('input', event.target)
+    })
+    window.onload = windowActions;
+
+    console.log(data)
+}
